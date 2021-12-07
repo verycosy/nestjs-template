3fbb3447b62abaf2e42b2a58802b30d0b63c418b
verycosy <verycosy@kakao.com>
2021-12-07 17:33:06 +0900

    add email, password column to user entity

--- libs/entity/src/domain/user/User.entity.ts
+++ libs/entity/src/domain/user/User.entity.ts
@@ -1,7 +1,13 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('user')
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 }
