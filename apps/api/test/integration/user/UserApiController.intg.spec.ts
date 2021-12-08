12366b6a60b18e1aad6bad9ebaa4437921af1b4a
verycosy <verycosy@kakao.com>
2021-12-08 17:55:16 +0900

    test user api controller with stub

--- apps/api/test/integration/user/UserApiController.intg.spec.ts
+++ apps/api/test/integration/user/UserApiController.intg.spec.ts
@@ -7,6 +7,7 @@ import { Repository } from 'typeorm';
 import { User } from '@app/entity/domain/user/User.entity';
 import { SignUpRequest } from '../../../../api/src/user/dto/SignUpRequest';
 import { BadRequestException } from '@nestjs/common';
+import { mock, when, instance } from 'ts-mockito';
 
 describe('UserApiController', () => {
   let sut: UserApiController;
@@ -43,6 +44,45 @@ describe('UserApiController', () => {
       request.email = 'test@test.com';
       request.password = 'password';
       request.confirmPassword = 'password';
+
+      const result = await sut.signUp(request);
+      expect(result.email).toEqual(request.email);
+    });
+  });
+
+  describe('signUp with ts-mockito', () => {
+    it('비밀번호가 서로 다르면 BadRequestException', async () => {
+      const request = new SignUpRequest();
+      request.password = 'password';
+      request.confirmPassword = 'confirmPassword';
+
+      const stubUserApiService = mock<UserApiService>();
+      sut = new UserApiController(stubUserApiService);
+
+      try {
+        await sut.signUp(request);
+      } catch (err) {
+        expect(err).toBeInstanceOf(BadRequestException);
+        expect(err.message).toEqual('Password does not matched');
+      }
+    });
+
+    it('회원가입 성공시 생성된 유저 정보 반환', async () => {
+      const request = new SignUpRequest();
+      request.email = 'test@test.com';
+      request.password = 'password';
+      request.confirmPassword = 'password';
+
+      const requestToEntity = request.toEntity();
+      request.toEntity = jest.fn(() => requestToEntity);
+
+      const mockUserApiService = mock<UserApiService>();
+      when(mockUserApiService.signUp(requestToEntity)).thenResolve({
+        id: 1,
+        email: request.email,
+        password: request.password,
+      });
+      sut = new UserApiController(instance(mockUserApiService));
 
       const result = await sut.signUp(request);
       expect(result.email).toEqual(request.email);
