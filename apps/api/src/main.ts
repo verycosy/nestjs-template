dd4dff4604b463cef2836df720b31b426998bdc6
verycosy <verycosy@kakao.com>
2021-12-17 16:04:03 +0900

    swagger setup

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ApiModule } from './api.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Template')
+    .setDescription('The NestJS API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
