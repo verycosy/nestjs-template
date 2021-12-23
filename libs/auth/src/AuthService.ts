67e0cbde88d3821ebee8f3131a05e3e5bf76d8ea
verycosy <verycosy@kakao.com>
2021-12-23 19:00:13 +0900

    add login funciton to auth service

--- libs/auth/src/AuthService.ts
+++ libs/auth/src/AuthService.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { JwtPayload } from './JwtPayload';
 
 @Injectable()
 export class AuthService {
@@ -10,4 +12,36 @@ export class AuthService {
     @InjectRepository(User) private readonly userRepository: Repository<User>,
     private readonly jwtService: JwtService,
   ) {}
+
+  async login(email: string, plainTextPassword: string) {
+    const user = await this.userRepository.findOne({ email });
+
+    if (!user) {
+      throw new Error(`User not found`);
+    }
+
+    if (!(await bcrypt.compare(plainTextPassword, user.password))) {
+      throw new Error(`User not found`);
+    }
+
+    const payload: JwtPayload = {
+      id: user.id,
+    };
+
+    const accessToken = await this.jwtService.signAsync(payload, {
+      secret: process.env.ACCESS_TOKEN_SECRET,
+      expiresIn: `${process.env.ACCESS_TOKEN_EXPIRES_IN}s`,
+    });
+
+    const refreshToken = await this.jwtService.signAsync(payload, {
+      secret: process.env.REFRESH_TOKEN_SECRET,
+      expiresIn: `${process.env.REFRESH_TOKEN_EXPIRES_IN}s`,
+    });
+
+    return {
+      user,
+      accessToken,
+      refreshToken,
+    };
+  }
 }
