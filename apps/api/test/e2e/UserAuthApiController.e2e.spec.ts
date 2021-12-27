9bdd57d81d55693764a6ad4ec57a3038b19c2a55
verycosy <verycosy@kakao.com>
2021-12-27 15:37:36 +0900

    add login, logout, refresh api e2e test

--- apps/api/test/e2e/UserAuthApiController.e2e.spec.ts
+++ apps/api/test/e2e/UserAuthApiController.e2e.spec.ts
@@ -8,7 +8,7 @@ describe('UserAuthApiController (e2e)', () => {
   let app: INestApplication;
   let cacheManager: Cache;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [ApiModule],
       providers: [],
@@ -20,25 +20,44 @@ describe('UserAuthApiController (e2e)', () => {
     cacheManager = module.get<Cache>(CACHE_MANAGER);
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
     await app.close();
     await cacheManager.reset();
   });
 
+  async function authSms(phoneNumber: string) {
+    const url = '/users/sms';
+
+    await request(app.getHttpServer()).post(url).send({
+      phoneNumber,
+    });
+
+    await request(app.getHttpServer()).patch(url).send({
+      phoneNumber,
+      authCode: '123456',
+    });
+  }
+
+  async function signUp(email: string, password: string, phoneNumber: string) {
+    await authSms(phoneNumber);
+
+    return await request(app.getHttpServer()).post('/users/sign-up').send({
+      name: 'verycosy',
+      email,
+      password,
+      phoneNumber,
+      confirmPassword: password,
+    });
+  }
+
+  async function login(email: string, password: string) {
+    return await request(app.getHttpServer()).post('/users/login').send({
+      email,
+      password,
+    });
+  }
+
   describe('/sign-up (POST)', () => {
-    async function authSms(phoneNumber: string) {
-      const url = '/users/sms';
-
-      await request(app.getHttpServer()).post(url).send({
-        phoneNumber,
-      });
-
-      await request(app.getHttpServer()).patch(url).send({
-        phoneNumber,
-        authCode: '123456',
-      });
-    }
-
     it('인증되지 않은 휴대폰 번호는 에러 메시지', async () => {
       const res = await request(app.getHttpServer())
         .post('/users/sign-up')
@@ -81,22 +100,97 @@ describe('UserAuthApiController (e2e)', () => {
     });
 
     it('가입된 유저 정보 반환', async () => {
+      const email = 'test@test.com';
+      const password = 'password';
       const phoneNumber = '010-1111-2222';
-      await authSms(phoneNumber);
 
-      const res = await request(app.getHttpServer())
-        .post('/users/sign-up')
-        .send({
-          name: 'verycosy',
-          email: 'test@test.com',
-          password: 'password',
-          phoneNumber,
-          confirmPassword: 'password',
-        });
+      const res = await signUp(email, password, phoneNumber);
 
       expect(res.statusCode).toEqual(201);
-      expect(res.body.email).toEqual('test@test.com');
-      expect(res.body.phoneNumber).toEqual('010-1111-2222');
+      expect(res.body.email).toEqual(email);
+      expect(res.body.phoneNumber).toEqual(phoneNumber);
     });
+  });
+
+  describe('/login (POST)', () => {
+    it('존재하지 않는 계정이면 error', async () => {
+      const res = await request(app.getHttpServer()).post('/users/login').send({
+        email: 'test@test.com',
+        password: 'passwood',
+      });
+
+      expect(res.statusCode).toEqual(500);
+    });
+
+    it('비밀번호가 일치하지 않으면 error', async () => {
+      const email = 'test@test.com';
+      const password = 'password';
+      const phoneNumber = '010-1111-2222';
+
+      await signUp(email, password, phoneNumber);
+
+      const res = await request(app.getHttpServer()).post('/users/login').send({
+        email,
+        password: 'passwood',
+      });
+
+      expect(res.statusCode).toEqual(500);
+    });
+
+    it('회원 정보와 jwt 토큰 반환', async () => {
+      const email = 'test@test.com';
+      const password = 'password';
+      const phoneNumber = '010-1111-2222';
+
+      await signUp(email, password, phoneNumber);
+      const { body, statusCode } = await login(email, password);
+
+      expect(statusCode).toEqual(201);
+      expect(body.user.email).toEqual(email);
+      expect(body.accessToken).toBeDefined();
+      expect(body.refreshToken).toBeDefined();
+    });
+  });
+
+  it('/logout (GET)', async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+    const phoneNumber = '010-1111-2222';
+
+    await signUp(email, password, phoneNumber);
+    const {
+      body: { accessToken },
+    } = await login(email, password);
+
+    const { statusCode } = await request(app.getHttpServer())
+      .get('/users/logout')
+      .set('Authorization', 'Bearer ' + accessToken);
+    expect(statusCode).toEqual(200);
+  });
+
+  it('/refresh (PATCH)', async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+    const phoneNumber = '010-1111-2222';
+
+    await signUp(email, password, phoneNumber);
+    const {
+      body: { refreshToken },
+    } = await login(email, password);
+
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(null);
+      }, 500);
+    });
+
+    const { body, statusCode } = await request(app.getHttpServer())
+      .patch('/users/refresh')
+      .set('Authorization', 'Bearer ' + refreshToken);
+
+    expect(statusCode).toEqual(200);
+    expect(body.accessToken).toBeDefined();
+    expect(body.refreshToken).toBeDefined();
+    expect(body.refreshToken).not.toEqual(refreshToken);
   });
 });
