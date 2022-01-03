3bc1040350f5cc1fc2b9edefee60bc6cc3610bbf
verycosy <verycosy@kakao.com>
2022-01-03 13:07:26 +0900

    feat: print env and log level

--- libs/config/src/getLoggerOptions.ts
+++ libs/config/src/getLoggerOptions.ts
@@ -1,6 +1,13 @@
 import { LoggerOptions, transports } from 'winston';
 
 export function getLoggerOptions(): LoggerOptions {
+  const nodeEnv = process.env.NODE_ENV;
+
+  const isLocalEnv = ['local', 'test', undefined].includes(nodeEnv);
+  const level = isLocalEnv ? 'debug' : 'info';
+
+  console.log(`nodeEnv=${nodeEnv}, level=${level}`);
+
   return {
     transports: [new transports.Console()],
     silent: process.env.NODE_ENV === 'test',
