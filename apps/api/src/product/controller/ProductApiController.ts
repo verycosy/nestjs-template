e659d742804e591b40ca6f3cb3be9c1a459ec190
Jinho Jang <verycosy@kakao.com>
2022-01-16 17:09:18 +0900

    feat: find product by id

---
+++ apps/api/src/product/controller/ProductApiController.ts
@@ -0,0 +1,26 @@
+import { ResponseEntity } from '@app/config/response';
+import { Product } from '@app/entity/domain/product/Product.entity';
+import { Controller, Get, Param } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Controller('/product')
+export class ProductApiController {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  @Get('/:id')
+  async findProduct(
+    @Param('id') id: number,
+  ): Promise<ResponseEntity<Product | string>> {
+    const product = await this.productRepository.findOne({ id });
+
+    if (!product) {
+      return ResponseEntity.ERROR_WITH('Product not found');
+    }
+
+    return ResponseEntity.OK_WITH(product);
+  }
+}
