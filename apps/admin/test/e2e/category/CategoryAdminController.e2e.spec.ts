2d6a5977ad49266879902eb4a87cf249e2ca7713
verycosy <verycosy@kakao.com>
2022-01-20 17:26:17 +0900

    test(e2e): /category/:id (POST)

--- apps/admin/test/e2e/category/CategoryAdminController.e2e.spec.ts
+++ apps/admin/test/e2e/category/CategoryAdminController.e2e.spec.ts
@@ -1,16 +1,18 @@
 import { AuthService } from '@app/auth';
 import { ResponseEntity } from '@app/config/response';
-import { Category } from '@app/entity/domain/category';
+import { Category, SubCategory } from '@app/entity/domain/category';
 import { getAdminAccessToken } from '@app/util/testing/getAdminAccessToken';
 import { ClassSerializerInterceptor, INestApplication } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+import { Repository } from 'typeorm';
 import { AdminModule } from '../../../../admin/src/AdminModule';
 
 describe('CategoryAdminController (e2e)', () => {
   let app: INestApplication;
   let accessToken: string;
+  let categoryRepository: Repository<Category>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,6 +26,7 @@ describe('CategoryAdminController (e2e)', () => {
 
     await app.init();
 
+    categoryRepository = module.get('CategoryRepository');
     accessToken = await getAdminAccessToken(module.get(AuthService));
   });
 
@@ -41,6 +44,23 @@ describe('CategoryAdminController (e2e)', () => {
 
     const body: ResponseEntity<Category> = res.body;
     expect(res.status).toBe(201);
+    expect(body.data.id).toBe(1);
     expect(body.data.name).toBe('fruit');
+  });
+
+  it('/category/:id (POST)', async () => {
+    await categoryRepository.save(new Category('fruit'));
+
+    const res = await request(app.getHttpServer())
+      .post('/category/1')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        name: 'tropics',
+      });
+
+    const body: ResponseEntity<SubCategory> = res.body;
+    expect(res.status).toBe(201);
+    expect(body.data.id).toBe(1);
+    expect(body.data.name).toBe('tropics');
   });
 });
